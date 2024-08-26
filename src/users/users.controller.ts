38c27a68c7bc260ece32d078a269ead833292f62
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2024-08-26 00:14:34 -0300

    chore: conclusão da implementação de usuários

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ReturnUserDto } from './dto/return-users.dto';
@@ -9,7 +9,7 @@ export class UsersController {
 
     @Post()
     async createAdminUser(
-        @Body() createUserDto: CreateUserDto,
+        @Body(ValidationPipe) createUserDto: CreateUserDto,
     ): Promise<ReturnUserDto> {
         const user = await this.usersService.createAdminUser(createUserDto);
         return {
