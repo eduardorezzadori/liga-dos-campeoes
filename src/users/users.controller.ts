e8e558823aa9218ecfe2aed615b035580d6edd7c
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2024-09-05 23:14:34 -0300

    chore: conclusão da implementação da autenticação

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ReturnUserDto } from './dto/return-users.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Post()
+    @UseGuards(AuthGuard())
     async createAdminUser(
         @Body(ValidationPipe) createUserDto: CreateUserDto,
     ): Promise<ReturnUserDto> {
