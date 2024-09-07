a094598c2ca202d3e62022caa38301edff1069b1
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2024-09-06 23:38:54 -0300

    chore: finalização do CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,11 @@
 import {
     Body,
     Controller,
+    Delete,
+    ForbiddenException,
+    Get,
+    Param,
+    Patch,
     Post,
     UseGuards,
     ValidationPipe
@@ -12,14 +17,17 @@ import { AuthGuard } from '@nestjs/passport';
 import { Role } from 'src/auth/role.decorator';
 import { UserRole } from './user-roles.enum';
 import { RolesGuard } from 'src/auth/roles.guards';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('users')
+@UseGuards(AuthGuard(), RolesGuard)
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Post()
     @Role(UserRole.ADMIN)
-    @UseGuards(AuthGuard(), RolesGuard)
     async createAdminUser(
         @Body(ValidationPipe) createUserDto: CreateUserDto,
     ): Promise<ReturnUserDto> {
@@ -27,6 +35,40 @@ export class UsersController {
         return {
             user,
             message: 'Administrador cadastrado com sucesso',
+        };
+    }
+
+    @Get(':id')
+    @Role(UserRole.ADMIN)
+    async findUserById(@Param('id') id): Promise<ReturnUserDto> {
+        const user = await this.usersService.findUserById(id);
+        return {
+            user,
+            message: 'Usuário encontrado',
+        };
+    }
+
+    @Patch(':id')
+    async updateUser(
+        @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+        @GetUser() user: User,
+        @Param('id') id: string,
+    ) {
+        if (user.role != UserRole.ADMIN && user.id.toString() != id) {
+            throw new ForbiddenException(
+                'Você não tem autorização para acessar esse recurso',
+            );
+        } else {
+            return this.usersService.updateUser(updateUserDto, id);
+        }
+    }
+
+    @Delete(':id')
+    @Role(UserRole.ADMIN)
+    async deleteUser(@Param('id') id: string) {
+        await this.usersService.deleteUser(id);
+        return {
+            message: 'Usuário removido com sucesso',
         };
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { UserRole } from './user-roles.enum';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -17,6 +18,44 @@ export class UsersService {
             throw new UnprocessableEntityException('As senhas não conferem');
         } else {
             return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
+        }
+    }
+
+    async findUserById(userId: string): Promise<User> {
+        const user = await User.findOne({
+            select: ['email', 'name', 'role', 'id'],
+            where: { id: userId }
+        });
+
+        if (!user) throw new NotFoundException('Usuário não encontrado');
+
+        return user;
+
+    }
+
+    async updateUser(updateUserDto: UpdateUserDto, id: string): Promise<User> {
+        const user = await this.findUserById(id);
+        const { name, email, role, status } = updateUserDto;
+        user.name = name ? name : user.name;
+        user.email = email ? email : user.email;
+        user.role = role ? role : user.role;
+        user.status = status === undefined ? user.status : status;
+        try {
+            await user.save();
+            return user;
+        } catch (error) {
+            throw new InternalServerErrorException(
+                'Erro ao salvar os dados no banco de dados',
+            );
+        }
+    }
+
+    async deleteUser(userId: string) {
+        const result = await User.delete({ id: userId });
+        if (result.affected === 0) {
+            throw new NotFoundException(
+                'Não foi encontrado um usuário com o ID informado',
+            );
         }
     }
 }
