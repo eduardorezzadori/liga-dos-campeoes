de80db9e8d37d023ff92bd32eb2dab45a9100e74
Eduardo Rezzadori <r.eduardo.23@outlook.com>
2024-09-09 23:48:00 -0300

    chore: criação dos logs, tem bug no console

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './configs/winston.configs';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 
 export class AppModule { }
